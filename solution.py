921d4cbb8346cd7aa7af73741cb99c5e0f4d4e9b
Catherine243 <129989270+Catherine243@users.noreply.github.com>
2023-04-13 23:52:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,8 @@ from statsmodels.stats.weightstats import ztest as ztest
 chat_id = 557932710 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool:
-p_value=ztest(x, value=300, alternative = 'smaller')[1]
-if (p_value < 0.08):
-return True
-else:
-return False
+    p_value=ztest(x, value=300, alternative = 'smaller')[1]
+    if (p_value < 0.08):
+    return True
+    else:
+    return False
